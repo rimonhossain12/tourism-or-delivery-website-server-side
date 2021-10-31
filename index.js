f66e374cc6993bc72f342378fd78885ebbfeb85c
rimonspi12 <rimonmpi@gmail.com>
2021-10-31 14:34:21 +0600

    server is connected with node mongodb

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const { response } = require('express');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygqbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// middle ware 
+app.use(cors());
+app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygqbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run () {
-    try{
+async function run() {
+    try {
         await client.connect();
-        console.log('database is connecting');
-    }
-    finally{
+        const database = client.db('travelSite');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services',async(req,res) =>{
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log(service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        });
+
+    } finally {
         // await client.close();
     }
 }
+
 run().catch(console.dir);
 
-
-app.get('/',(req,res) => {
-    res.send('I am learning node js')
+app.get('/', (req, res) => {
+    res.send('travel node js server')
 })
 
-app.listen(port,() => {
-    console.log('Running on port');
+app.listen(port, () => {
+    console.log('Running travel server on port',port);
 })
