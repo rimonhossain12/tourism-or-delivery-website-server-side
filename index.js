dc8603aefc4f2cdec3bfb5f8509d9233b2a5c213
rimonspi12 <rimonmpi@gmail.com>
2021-12-15 13:51:28 +0600

    user orders stored and user products load

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { response } = require('express');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const { signal } = require('nodemon/lib/config/defaults');
+const { reset } = require('nodemon');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,24 +23,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        // console.log('database is connecting');
         const database = client.db('travelSite');
-        const servicesCollection = database.collection('services')
-            ;
+        const servicesCollection = database.collection('services');
         const ordersCollection = database.collection('userOrder');
+
+
         // GET API
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
         })
+
         // get single service
         app.get('/single/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const singleService = await servicesCollection.findOne(query);
             res.json(singleService);
+        });
+        // find date using email
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const cursor = ordersCollection.find({ email });
+            const products = await cursor.toArray();
+            res.send(products);
         })
+
         // POST API
         app.post('/services', async (req, res) => {
             const service = req.body;
@@ -50,14 +61,19 @@ async function run() {
             const data = req.body;
             console.log(data);
             const result = await ordersCollection.insertOne(data);
-            console.log('data is added with result', result);
             res.json(result);
         });
-        // get data for user email 
-        app.get('/myOrder', async (req, res) => {
-            console.log('hitting the my order api');
-            res.send('Hello user email api');
+
+        // delete user order
+        app.delete('/cancel/:id',async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            console.log('data is deleted',result);
+            res.json(result);
+            
         })
+
     } finally {
         // await client.close();
     }
